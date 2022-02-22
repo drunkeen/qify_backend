db3010836a6c11813e3fca9c5938eac51e913626
erwan.vivien <erwan.vivien@epita.fr>
2022-02-22 10:06:57 +0100

    fix: using macro to generate formated const str

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -15,17 +15,40 @@ pub struct GenericOutput<T: Serialize> {
     pub error: Option<String>,
 }
 
-#[allow(dead_code)]
-pub const SERDE_ERROR: &str = r#"JSON: Error converting to string"#; // 500
+macro_rules! error_string {
+    ($s:expr, $m:expr) => {
+        concat!(
+            r#"{"data":null,"success":false,"status_code":"#,
+            $s,
+            r#","error":""#,
+            $m,
+            "\"}"
+        )
+    };
+}
 
+/// status_code: `500`
 #[allow(dead_code)]
-pub const NOT_IMPLEMENTED_RELEASE_MODE: &str = r#"Not implemented in release mode"#; // 501
+pub const SERDE_ERROR: &str = error_string!(500, r#"JSON: Error converting to string"#);
 
+/// status_code: `501`
 #[allow(dead_code)]
-pub const INTERNAL_SERVER_ERROR: &str = r#"INTERNAL_SERVER_ERROR"#; // 500
+pub const NOT_IMPLEMENTED_RELEASE_MODE: &str =
+    error_string!(501, r#"Not implemented in release mode"#);
 
+/// status_code: `500`
 #[allow(dead_code)]
-pub const SPOTIFY_API_FORBIDDEN: &str = r#"Non-premium accounts can't access Spotify API"#; // 403
+pub const INTERNAL_SERVER_ERROR: &str = error_string!(500, r#"INTERNAL_SERVER_ERROR"#);
+
+/// status_code: `403`
+#[allow(dead_code)]
+pub const SPOTIFY_API_FORBIDDEN: &str =
+    error_string!(403, r#"Non-premium accounts can't access Spotify API"#);
+
+/// status_code: `400`
+#[allow(dead_code)]
+pub const SPOTIFY_API_SEARCH_MISSING_FIELDS: &str =
+    error_string!(400, r#"There are missing fields"#);
 
 #[test]
 fn generic_output_serialize() {
